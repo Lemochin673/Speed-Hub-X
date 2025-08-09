67fca6dc875d948abae89f5346688b4f3ba986d2
Lemochin673 <sigmackiper12345@gmail.com>
2025-08-09 14:59:43 +0500

    Update Speed-Hub-X.lua

--- Speed-Hub-X.lua
+++ Speed-Hub-X.lua
@@ -1,4 +1,3 @@
 ID="e225750c-deff-47e5-b1b7-bfa51ee84111";
 loadstring(game:HttpGet("https://cdn.reverse.software/loader.luau"))()
-wait(1)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/veryimportantrr/x/refs/heads/main/gag_visual.lua", true))
+
